e7033e05ea827f3e5ef6bb8784033678814a0d31
root <root@taroppi0122>
2024-07-13 23:07:37 +0900

    first commit

---
+++ src/resources/views/layouts/app.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset ('css/sanitize.css') }}">
+    <link rel="stylesheet" href="{{ asset ('css/app.css') }}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inika:wght@400;700&display=swap" rel="stylesheet">
+    @yield('css')
+</head>
+
+<body>
+    <header class="header">
+        <div class="header__inner">
+            <h1 class="header__site-title inika-regular">FashionablyLate</h1>
+            <nav class="nav">
+                <ul>
+                    <li>aaa</li>
+                    <li>bbb</li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+    <main>
+        @yield('content')
+    </main>
+</body>
+
+</html>
